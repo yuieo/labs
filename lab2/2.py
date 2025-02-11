dc3a0eebb8b7d6aaa8f8ba3cc3660522dcdcdc1e
yuieo <yusif.gadziev@gmail.com>
2025-02-11 10:56:01 +0500

    test

--- lab2/2.py
+++ lab2/2.py
@@ -1,1 +1,11 @@
-print("hi2")
+#вариант 1
+#задание 1
+
+import itertools
+k = 0
+a = list(itertools.product('ТИМОФЕЙ', repeat=5))
+for x in a:
+    if x.count('Й') == 1 and x[0]!='Й' and x[4]!='Й' and x.count('ЙИ')==0 and x.count('ИЙ')==0:
+        k+=1
+
+print(k)
