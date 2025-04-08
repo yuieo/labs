8971705444b804c9c7f8e912f3677632f70ebfff
yuieo <160876826+yuieo@users.noreply.github.com>
2025-04-08 10:23:52 +0500

    8th

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+
+def simple_random(min_val, max_val):
+    seed = simple_random.seed
+    seed = (1664525 * seed + 1013904223) & 0xFFFFFFFF
+    simple_random.seed = seed
+    return min_val + (seed % (max_val - min_val + 1))
+
+simple_random.seed = int(time.time())
+
+def count_divisors(num):
+    if num < 1:
+        return 0
+    count = 0
+    for i in range(1, int(num**0.5) + 1):
+        if num % i == 0:
+            count += 1
+            if i != num // i:
+                count += 1
+    return count
+
+min_val = 100
+max_val = 500
+n = 4
+
+numbers = [simple_random(min_val, max_val) for _ in range(6)]
+
+filtered_numbers = list(filter(lambda x: count_divisors(x) <= n, numbers))
+
+print("Сгенерированные числа:", numbers)
+print("После фильтрации:", filtered_numbers)
