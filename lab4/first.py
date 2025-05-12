f16e9acc902afbe2bbf0764011f0f0eef4f15e79
yuieo <160876826+yuieo@users.noreply.github.com>
2025-05-13 00:42:06 +0500

    iou2314

--- lab4/first.py
+++ lab4/first.py
@@ -1,37 +1,40 @@
-def repeat(times):
-    def decorator(func):
-        def wrapper(arg):
-            results = []
-            for _ in range(times):
-                result = func(arg)
-                results.append(result)
-            return results
+def logger(func=None, *, verbose=True):
+    if func is not None:
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if verbose:
+                print(f"Текущий результат: {result}")
+            return result
         return wrapper
-    return decorator
+    else:
+        def decorator(f):
+            return logger(f, verbose=verbose)
+        return decorator
+
 
 def make_calc(operation, initial=0):
-    result = initial  # начальное значение сохраняется в замыкании
-    
-    def calculator(value):
-        nonlocal result  # позволяет менять значение из замыкания
-        
+    result = initial
+
+    @logger(verbose=True)
+    def calculator(number):
+        nonlocal result
         if operation == "+":
-            result += value
+            result += number
         elif operation == "-":
-            result -= value
+            result -= number
         elif operation == "*":
-            result *= value
+            result *= number
         elif operation == "/":
-            result /= value
-            
+            result /= number
         return result
-    
+
     return calculator
 
-calc = make_calc("*", initial=1)
 
-calc = repeat(3)(calc)
+calc1 = make_calc("*", initial=1)
+calc1(5)  # 5
+calc1(2)  # 10
 
-# Тест
-print(calc(5)) 
-print(calc(2))
+calc2 = make_calc("+", initial=0)
+calc2(3)  # 3
+calc2(4)  # 7
