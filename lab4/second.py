1a9a608b8c6f91f99206de62a0baeca09960c4a1
yuieo <160876826+yuieo@users.noreply.github.com>
2025-05-22 14:53:07 +0500

    yrtutyrurtyu

--- lab4/second.py
+++ lab4/second.py
@@ -1,32 +1,25 @@
-from functools import wraps
-from typing import Callable, Any, Optional
-
-
-def repeat(times: Optional[int] = 1):
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*args, **kwargs) -> list[Any]:
+def repeat(times=1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
             results = []
             for _ in range(times):
                 results.append(func(*args, **kwargs))
             return results
-
         return wrapper
-
     return decorator
 
-
+#
 @repeat(times=3)
-def add(a: int, b: int) -> int:
+def add(a, b):
     return a + b
 
 
-print(add(2, 3))
+print(add(2, 3))  #[5, 5, 5]
 
-
+##
 @repeat()
-def greet(name: str) -> str:
+def greet(name):
     return f"Привет, {name}!"
 
 
-print(greet("Юсиф"))
+print(greet("Юсиф"))  #['Привет, Юсиф!']
