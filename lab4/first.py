1146fde3726af00db7a02fdc59b65ba8fb689362
yuieo <160876826+yuieo@users.noreply.github.com>
2025-05-29 10:56:57 +0500

    tttyutyutyuytu

--- lab4/first.py
+++ lab4/first.py
@@ -2,7 +2,7 @@ def logger(func=None, *, verbose=True):
     if func is not None:
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            if verbose:
+            if verbose and not kwargs.get('_recursive', False):
                 print(f"Текущий результат: {result}")
             return result
         return wrapper
@@ -16,7 +16,7 @@ def make_calc(operation, initial=0):
     result = initial
 
     @logger(verbose=True)
-    def calculator(number):
+    def calculator(number, _recursive=False):
         nonlocal result
         if operation == "+":
             result += number
@@ -38,3 +38,15 @@ calc1(2)  # 10
 calc2 = make_calc("+", initial=0)
 calc2(3)  # 3
 calc2(4)  # 7
+
+
+def recursive_example(n):
+    calc = make_calc("+")
+    if n == 0:
+        return calc(0, _recursive=True)
+    return calc(n + recursive_example(n-1), _recursive=(n >= 1))
+
+
+print('рекурсия от n до 0')
+n = recursive_example(4)  # 10
+print(n)
